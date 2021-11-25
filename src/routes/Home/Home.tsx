70404c18e94ad697496a25a8c2195d76999bdc8a
marcosst17 <marcosstx17@gmail.com>
2021-11-25 14:24:53 -0300

    Landing done

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { HomeHeader } from "../../components/HomeComponents/HomeHeader/HomeHeader";
 import { HomeImages } from "../../components/HomeComponents/HomeImages/HomeImages";
 import { HomeButton } from "../../components/HomeComponents/HomeButton/HomeButton";
+import { HomeClients } from "../../components/HomeComponents/HomeClients/HomeClients";
 export const Home: React.FC = () => {
 
     return (
         <>
             <HomeHeader />
+            <HomeClients/>
             <HomeImages/>
             <HomeButton/>
         </>
