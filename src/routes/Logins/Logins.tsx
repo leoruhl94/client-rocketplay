bfeddb11b4a30e1498d99a882f5cb64e87942bde
leoruhl94 <leoruhl94@gmail.com>
2021-11-29 11:32:52 -0300

    .

--- src/routes/Logins/Logins.tsx
+++ src/routes/Logins/Logins.tsx
@@ -1,13 +1,11 @@
 import React, {useState} from "react";
-import { Link } from "react-router-dom";
-import google from "../../images/google.png";
 import {useHistory} from 'react-router-dom'
 import {GoogleLogin} from 'react-google-login'
 import axios from 'axios'
 
 import "./Logins.scss";
 import { NavigationMobile } from "../../containers/NavigationMobile/NavigationMobile";
-import { SuperButton } from "../../components/Buttons/SuperButton/SuperButton";
+
 import { Icon } from "../../components/Icon/Icon";
 //import { useGoogleLogin } from 'react-google-login'
 
@@ -58,10 +56,6 @@ export const Logins: React.FC = () => {
           <Icon svg="logoDarkColor" />
         </div>      
         <div className="singleButton">
-          {/* <button className="loginGoogle" onClick={handleSend}>
-           { sign ? "Sign up with google ": "Log in with google "}
-            <img className="logoGoogle" src={google} alt="google" />
-          </button> */}
           <GoogleLogin
             clientId="1009538709316-mp0t7rds0snem49ajha6d8u74mbgtb9v.apps.googleusercontent.com"
             buttonText={ sign ? "Sign up with google ": "Log in with google "}
@@ -76,16 +70,7 @@ export const Logins: React.FC = () => {
         { sign ? "Already have an account? Log in": "Don’t have an account? Sign up"}
         </button>
 
-{/* 
-        <div className="Logs_buttons">
-          <SuperButton
-            name={"button_Login"}
-            route="/"
-            text="Log In with Google "
-            classes="loginGoogle"
-          />
-        </div> */}
- 
+
 
         <NavigationMobile />
       </div>
