c48ec61bf10af7836ec4195e6ab6e6d9f63eeb75
marcosst17 <marcosstx17@gmail.com>
2021-12-06 01:34:41 -0300

    About to merge

--- src/containers/NavigationTop/NavigationTop.tsx
+++ src/containers/NavigationTop/NavigationTop.tsx
@@ -8,7 +8,7 @@ export const NavigationTop: React.FC = () => {
 
   return (
     <section className="navigationTop">
-        <NavLink className="navigationTop__navLink navigationTop__navLink_onlyDesktop" to="/">Home</NavLink>
+        {/* <NavLink className="navigationTop__navLink navigationTop__navLink_onlyDesktop" to="/">Home</NavLink> */}
         <NavLink className="navigationTop__navLink " to="/login">Log In / Sign Up</NavLink>
     </section>
   );
