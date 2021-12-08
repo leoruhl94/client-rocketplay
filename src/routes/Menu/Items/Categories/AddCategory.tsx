f09b9186d531c90fa6809d4121e6de8573bc1bd9
Elias Lautaro Delgado <82496172+DelgadoElias@users.noreply.github.com>
2021-12-07 23:26:59 -0300

    Logger
    La categoría no se puede repetir si se escribe un nombre igual

--- src/routes/Menu/Items/Categories/AddCategory.tsx
+++ src/routes/Menu/Items/Categories/AddCategory.tsx
@@ -1,22 +1,35 @@
 import React, { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router";
 import { postCategory } from "../../../../redux/actions";
 
 //Estilización
 import './addCategory.scss'
+import { Categories } from './hardcode';
+import { storeState } from '../../../../redux/type';
 
 export const AddCategory : React.FC = () => {
     
     const [input, setInput] = useState<string>('');
+    const [logger, setLogger] = useState<string>('');
+
     const dispatch = useDispatch()
+
     let history = useHistory();
+    const categories : Categories[] = useSelector((state : storeState) => state.categories)
 
     function handleUpload(e){
         //Complete: Mandar al state por ahora
         e.preventDefault()
-        dispatch(postCategory(input))
-        history.goBack()
+        const even = (x) => input === x.title
+        if(categories.some(even)){
+            setLogger('The category already exists');
+            return;
+        }
+        else{
+            dispatch(postCategory(input))
+            history.goBack()
+        }
     }
     function handleData(e){
         //Complete: Subir todo
@@ -32,10 +45,13 @@ export const AddCategory : React.FC = () => {
             {/* Complete: Tomar nombre */}
             {/* Neh: Tomar videos para poner categorías */}
             <form onSubmit={handleUpload}>
-                <input className="Add__Input-text" placeholder="Name..." onChange={handleData} type="text"required />
+                <input className="Add__Input-text" placeholder="Name..." onChange={handleData} type="text" required />
                 <input className="Add__Input-btn" type="submit" />
             </form>
 
+            <div className="Add__Category-logger">
+                {logger}
+            </div>
 
             </article>)
 }
