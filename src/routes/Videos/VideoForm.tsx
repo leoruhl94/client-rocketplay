de4cdf59471c7abbae5f1f235f8420833ea2559b
marcosst17 <marcosstx17@gmail.com>
2021-12-09 18:53:12 -0300

    Made some progress with backend routes and some fixes in videoForm

--- src/routes/Videos/VideoForm.tsx
+++ src/routes/Videos/VideoForm.tsx
@@ -8,7 +8,7 @@ import { Icon } from "../../components/Icon/Icon";
 import { storeState } from "../../redux/type";
 import { Upload } from "@aws-sdk/lib-storage"
 import { S3Client, S3 } from "@aws-sdk/client-s3";
-import { s3 } from "s3"
+
 // Interfaces
 
 interface User {
