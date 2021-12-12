1d41ab71d2b7b92482e37fb6fba02d60ad6804b4
leoruhl94 <leoruhl94@gmail.com>
2021-12-12 16:26:14 -0300

    search video

--- src/routes/SearchMenu/SearchMenu.tsx
+++ src/routes/SearchMenu/SearchMenu.tsx
@@ -1,54 +1,89 @@
 import React from "react";
 import { SearchBar } from "../../components/SearchBar/SearchBar";
 import { MenuToggleContainer } from "../../components/MenuToggleContainer/MenuToggleContainer";
-import './SearchMenu.scss'
+import "./SearchMenu.scss";
 import { URL_BASE } from "../../constants/constants";
-import axios from 'axios'
+import axios from "axios";
+import { useState } from "react";
+import { useAuth } from "../../auth/useAuth";
 
-// const handleSubmit = async (e) => {
-//     e.preventDefault();
-   
-//     let res = await axios.get(`${URL_BASE}/searchBar`, {
-//       params: { schemaName:, title: },
-//     });
-   
-//   };
 export const SearchMenu: React.FC = () => {
-  
+  const auth = useAuth();
+  const [schemaName, setSchemaName] = useState(null);
+  const [videos, setVideos] = useState([]);
+  const handleSubmit = async (value) => {
+    console.log(schemaName);
+    if (!schemaName) {
+      console.log("gatito");
+    } else {
+      let res = await axios.get(`${URL_BASE}/searchBar`, {
+        params: { schemaName, title: value },
+      });
+      setVideos(res.data);
+    }
+  };
 
+  const handleSelect = (e) => {
+    e.preventDefault();
+    setSchemaName(e.target.value);
+  };
 
-    
-    return(
-        <MenuToggleContainer>
-
-            <SearchBar handler={()=>console.log("")}/>
+  return (
+    <MenuToggleContainer>
+      <select onChange={handleSelect} name="schemaName" id="">
+        <option value="all">Workspaces</option>
+        {auth?.user?.workspacesTitles?.map((w, i) => (
             
-        </MenuToggleContainer>
-     )
-}
-
-
+          <option key={w} value={auth?.user?.workspaces?.length ? auth.user.workspaces[i] : ""}>
+            {w}
+          </option>
+        ))}
+      </select>
+      <SearchBar handler={handleSubmit} />
+      <div className="">
+        {videos?.map((v) => (
+          <VideoItem
+            videoid="aa"
+            thumbnail="s"
+            title="titletitletitle"
+            category="category"
+            channelName="channelName"
+          />
+        ))}
+      </div>
+    </MenuToggleContainer>
+  );
+};
 
 import "./VideoItem.scss";
 import { Link } from "react-router-dom";
 import { Icon } from "src/components/Icon/Icon";
 
 interface Props {
-    videoid: string;
-    thumbnail: string;
-    title: string;
-    category: string;
-    channelName: string;
+  videoid: string;
+  thumbnail: string;
+  title: string;
+  category: string;
+  channelName: string;
 }
-const VideoItem: React.FC<Props> = ({ videoid, thumbnail, title, category , channelName}) => {
+const VideoItem: React.FC<Props> = ({
+  videoid,
+  thumbnail,
+  title,
+  category,
+  channelName,
+}) => {
   return (
     <Link to={`/home/${videoid}`} className="VideoItem">
       <div className="VideoItem__image_container">
-        <img className="VideoItem__image" src={`https://rocketplay2021.s3.us-east-1.amazonaws.com/${thumbnail}`}/>
+        <img
+          className="VideoItem__image"
+          src={`https://rocketplay2021.s3.us-east-1.amazonaws.com/${thumbnail}`}
+        />
       </div>
       <div className="VideoItem__info">
-      <h2 className="VideoItem__title">{title}</h2>
-        <p className="VideoItem__channel_categorie">{`${category} > ${channelName}`}</p>
+        <h2 className="VideoItem__title">{title}</h2>
+        <p className="VideoItem__channel_category">{`${channelName} > ${category}`}</p>
       </div>
     </Link>
   );
