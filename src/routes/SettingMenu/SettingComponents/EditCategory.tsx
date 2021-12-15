1fe2d85afa6f8bb3def2e8c886c54dd758eb715e
leoruhl94 <leoruhl94@gmail.com>
2021-12-15 13:51:59 -0300

    styles channels y categories settings

--- src/routes/SettingMenu/SettingComponents/EditCategory.tsx
+++ src/routes/SettingMenu/SettingComponents/EditCategory.tsx
@@ -2,8 +2,6 @@ import React, { useState } from "react";
 import axios from "axios";
 import { URL_BASE } from "../../../constants/constants";
 import { useAuth } from "../../../auth/useAuth";
-import { auth } from "googleapis/build/src/apis/apikeys";
-
 
 interface InfoSubmit {
   schemaName: string;
@@ -21,15 +19,14 @@ interface Channels {
 }
 interface SchemaName {
   name: string;
-  
 }
 interface openRemove {
   divClass: string;
   buttonDisabled: boolean;
 }
 export const EditCategory: React.FC = () => {
-  const auth = useAuth()
-  const [schemaName, setSchemaName] = useState<SchemaName>()
+  const auth = useAuth();
+  const [schemaName, setSchemaName] = useState<SchemaName>();
   const [channelsState, setChannelsState] = useState<Channels[]>();
   const [categoryState, setCategoryState] = useState<Categories[]>();
   const [infoSubmit, setInfoSubmit] = useState<InfoSubmit>({
@@ -38,97 +35,107 @@ export const EditCategory: React.FC = () => {
     newName: "",
   });
 
-const [openRemove, setOpenRemove] = useState(
-  {
+  const [openRemove, setOpenRemove] = useState({
     divClass: "remove-channel-div display__none",
-    buttonDisabled: true
-  }
-);
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  if (!infoSubmit.schemaName) {
-    console.log("sale conejito porque no hay esquema")
-  } else {
-    await axios.put(`${URL_BASE}/category`, infoSubmit)
-  }
-  setInfoSubmit({
-    schemaName: "",
-    categoryId: "",
-    newName: "",
-  })
-}
-
-const handleWorkspaceSelect = (e) => {
-  e.preventDefault();
-  setSchemaName(e.target.value)
-  categoryState ? setCategoryState([]) : null;
-  setInfoSubmit({
-    schemaName: e.target.value,
-    categoryId: "",
-    newName: "",
+    buttonDisabled: true,
   });
-  axios.get(`${URL_BASE}/channels`, { params: { schemaName: e.target.value } })
-      .then((r) => {
-      let array: any[] = [];
-      r.data.map((el) => {
-        let obj = {
-          name: el.name,
-          id: el.id,
-        };
-        array.push(obj);
-      });
-      setChannelsState(array);
-      setInfoSubmit({ ...infoSubmit, schemaName: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!infoSubmit.schemaName) {
+      console.log("sale conejito porque no hay esquema");
+    } else {
+      await axios.put(`${URL_BASE}/category`, infoSubmit);
+    }
+    setInfoSubmit({
+      schemaName: "",
+      categoryId: "",
+      newName: "",
     });
-};
+  };
 
-const handleChannelSelect = (e) => {
-  e.preventDefault();
-  const array = e.target.value.split("%-%")
+  const handleWorkspaceSelect = (e) => {
+    e.preventDefault();
+    setSchemaName(e.target.value);
+    categoryState ? setCategoryState([]) : null;
+    setInfoSubmit({
+      schemaName: e.target.value,
+      categoryId: "",
+      newName: "",
+    });
+    axios
+      .get(`${URL_BASE}/channels`, { params: { schemaName: e.target.value } })
+      .then((r) => {
+        let array: any[] = [];
+        r.data.map((el) => {
+          let obj = {
+            name: el.name,
+            id: el.id,
+          };
+          array.push(obj);
+        });
+        setChannelsState(array);
+        setInfoSubmit({ ...infoSubmit, schemaName: e.target.value });
+      });
+  };
 
-  axios.get(`${URL_BASE}/category/bychannel`, {params: {schemaName: schemaName, channelId: array[1]}})
-  .then(r => {
-    let array1:any[] = []
-    r.data.map(el => {
-      let obj = {
-        name: el.catName,
-        id: el.catId,
-      }
-      array1.push(obj)
-      setCategoryState(array1)
-    })
-  })
+  const handleChannelSelect = (e) => {
+    e.preventDefault();
+    const array = e.target.value.split("%-%");
 
-}
+    axios
+      .get(`${URL_BASE}/category/bychannel`, {
+        params: { schemaName: schemaName, channelId: array[1] },
+      })
+      .then((r) => {
+        let array1: any[] = [];
+        r.data.map((el) => {
+          let obj = {
+            name: el.catName,
+            id: el.catId,
+          };
+          array1.push(obj);
+          setCategoryState(array1);
+        });
+      });
+  };
 
-const handleCategorySelect = (e) => {
-  e.preventDefault();
-  setInfoSubmit({ ...infoSubmit, categoryId: e.target.value });
-};
+  const handleCategorySelect = (e) => {
+    e.preventDefault();
+    setInfoSubmit({ ...infoSubmit, categoryId: e.target.value });
+  };
 
-const handleOnChange = (e) => {
-  e.preventDefault()
-  setInfoSubmit({...infoSubmit, newName: e.target.value})
-}
+  const handleOnChange = (e) => {
+    e.preventDefault();
+    setInfoSubmit({ ...infoSubmit, newName: e.target.value });
+  };
 
-const handleShow = (e) => {
-  e.preventDefault();
-  let div = document.querySelector(".remove-channel-div");
-  setOpenRemove({...openRemove, divClass: "remove-channel-div"});
-};
+  const handleShow = (e) => {
+    e.preventDefault();
+    let div = document.querySelector(".remove-channel-div");
+    setOpenRemove({ ...openRemove, divClass: "remove-channel-div" });
+  };
 
-return(
+  return (
     <div>
-       <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit}>
         <div className="Settings__selects">
-          <select onChange={handleWorkspaceSelect} name="schemaName" id="" className="SelectComponent">
-            <option value="all" className="SelectComponent_option">Workspaces</option>
+          <select
+            onChange={handleWorkspaceSelect}
+            name="schemaName"
+            id=""
+            className="SelectComponent Select__100w"
+          >
+            <option value="all" className="SelectComponent_option">
+              Workspaces
+            </option>
             {auth?.user?.myWorkspaces?.map((w, i) => (
               <option
                 key={i}
                 value={
-                  auth?.user?.myWorkspaces?.length ? auth.user.myWorkspaces[i].name : ""
+                  auth?.user?.myWorkspaces?.length
+                    ? auth.user.myWorkspaces[i].name
+                    : ""
                 }
                 className="SelectComponent_option"
               >
@@ -136,11 +143,24 @@ return(
               </option>
             ))}
           </select>
+        </div>
+        <div className="Settings__selects">
           {channelsState?.length ? (
-            <select onChange={handleChannelSelect} name="oldName" id="" className="SelectComponent">
-              <option value="all" className="SelectComponent_option">Channels</option>
+            <select
+              onChange={handleChannelSelect}
+              name="oldName"
+              id=""
+              className="SelectComponent Select__50w"
+            >
+              <option value="all" className="SelectComponent_option">
+                Channels
+              </option>
               {channelsState?.map((ch) => (
-                <option key={ch.id} value={ch.name + "%-%" + ch.id} className="SelectComponent_option">
+                <option
+                  key={ch.id}
+                  value={ch.name + "%-%" + ch.id}
+                  className="SelectComponent_option"
+                >
                   {ch.name}
                 </option>
               ))}
@@ -166,21 +186,22 @@ return(
               ))}
             </select>
           ) : null}
-          </div>
-          <div className="Settings__inputs_container">
-            <label className="Settings__label">New name: </label>
-            <input
-              autoComplete="off"
-              type="text"
-              name="newName"
-              value={infoSubmit.newName}
-              onChange={handleOnChange}
-              className="Settings__input"
-            />
-            <button type="submit" className='Settings__button'>Edit Category</button>
-          </div>
-        
+        </div>
+        <div className="Settings__inputs_container">
+          <label className="Settings__label">New name: </label>
+          <input
+            autoComplete="off"
+            type="text"
+            name="newName"
+            value={infoSubmit.newName}
+            onChange={handleOnChange}
+            className="Settings__input"
+          />
+          <button type="submit" className="Settings__button">
+            Edit Category
+          </button>
+        </div>
       </form>
-</div>
-)
-}
+    </div>
+  );
+};
--- src/routes/SettingMenu/SettingComponents/RemoveCategory.tsx
+++ src/routes/SettingMenu/SettingComponents/RemoveCategory.tsx
@@ -2,8 +2,6 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { URL_BASE } from "../../../constants/constants";
 import { useAuth } from "../../../auth/useAuth";
-import { auth } from "googleapis/build/src/apis/apikeys";
-
 
 interface InfoSubmit {
   schemaName: string;
@@ -21,15 +19,14 @@ interface Channels {
 }
 interface SchemaName {
   name: string;
-  
 }
 interface openRemove {
   divClass: string;
   buttonDisabled: boolean;
 }
 export const RemoveCategory: React.FC = () => {
-  const auth = useAuth()
-  const [schemaName, setSchemaName] = useState<SchemaName>()
+  const auth = useAuth();
+  const [schemaName, setSchemaName] = useState<SchemaName>();
   const [channelsState, setChannelsState] = useState<Channels[]>();
   const [categoryState, setCategoryState] = useState<Categories[]>();
   const [infoSubmit, setInfoSubmit] = useState<InfoSubmit>({
@@ -38,114 +35,126 @@ export const RemoveCategory: React.FC = () => {
     status: "deleted",
   });
 
-const [openRemove, setOpenRemove] = useState(
-  {
+  const [openRemove, setOpenRemove] = useState({
     divClass: "remove-channel-div display__none",
-    buttonDisabled: true
-  }
-);
-
-const handleWorkspaceSelect = (e) => {
-  e.preventDefault();
-  setSchemaName(e.target.value)
-  categoryState ? setCategoryState([]) : null;
-  setInfoSubmit({
-    schemaName: e.target.value,
-    categoryId: "",
-    status: "deleted",
+    buttonDisabled: true,
   });
-  axios.get(`${URL_BASE}/channels`, { params: { schemaName: e.target.value } })
-      .then((r) => {
-      let array: any[] = [];
-      r.data.map((el) => {
-        let obj = {
-          name: el.name,
-          id: el.id,
-        };
-        array.push(obj);
-      });
-      setChannelsState(array);
-      setInfoSubmit({ ...infoSubmit, schemaName: e.target.value });
+
+  const handleWorkspaceSelect = (e) => {
+    e.preventDefault();
+    setSchemaName(e.target.value);
+    categoryState ? setCategoryState([]) : null;
+    setInfoSubmit({
+      schemaName: e.target.value,
+      categoryId: "",
+      status: "deleted",
     });
-};
+    axios
+      .get(`${URL_BASE}/channels`, { params: { schemaName: e.target.value } })
+      .then((r) => {
+        let array: any[] = [];
+        r.data.map((el) => {
+          let obj = {
+            name: el.name,
+            id: el.id,
+          };
+          array.push(obj);
+        });
+        setChannelsState(array);
+        setInfoSubmit({ ...infoSubmit, schemaName: e.target.value });
+      });
+  };
 
-//---------------------REMOVE CATEGORY-----------------------------------------------------
+  //---------------------REMOVE CATEGORY-----------------------------------------------------
 
-const handleSubmit = async (e) => {
-  e.preventDefault();
-  if (!infoSubmit.schemaName) {
-    console.log("sale conejito porque no hay esquema");
-  } else {
-    await axios.put(`${URL_BASE}/category/status`, infoSubmit);
-  }
-  setInfoSubmit({
-    schemaName: "",
-    categoryId: "",
-    status: "deleted",
-  });
-  setOpenRemove({...openRemove, divClass: "remove-channel-div display__none"})
-};
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!infoSubmit.schemaName) {
+      console.log("sale conejito porque no hay esquema");
+    } else {
+      await axios.put(`${URL_BASE}/category/status`, infoSubmit);
+    }
+    setInfoSubmit({
+      schemaName: "",
+      categoryId: "",
+      status: "deleted",
+    });
+    setOpenRemove({
+      ...openRemove,
+      divClass: "remove-channel-div display__none",
+    });
+  };
 
-const handleShow = (e) => {
-  e.preventDefault();
-  let div = document.querySelector(".remove-channel-div");
-  // div && div.setAttribute("className", "remove-channel-div")
-  setOpenRemove({...openRemove, divClass: "remove-channel-div"});
-  // Aca hacer q el div de elminar aparezca
-};
+  const handleShow = (e) => {
+    e.preventDefault();
+    let div = document.querySelector(".remove-channel-div");
+    // div && div.setAttribute("className", "remove-channel-div")
+    setOpenRemove({ ...openRemove, divClass: "remove-channel-div" });
+    // Aca hacer q el div de elminar aparezca
+  };
 
+  const handleChannelSelect = (e) => {
+    e.preventDefault();
+    const array = e.target.value.split("%-%");
 
+    axios
+      .get(`${URL_BASE}/category/bychannel`, {
+        params: { schemaName: schemaName, channelId: array[1] },
+      })
+      .then((r) => {
+        console.log(r.data);
+        let array1: any[] = [];
+        r.data.map((el) => {
+          let obj = {
+            name: el.catName,
+            id: el.catId,
+          };
+          array1.push(obj);
+        });
+        setCategoryState(array1);
+      });
+  };
 
-const handleChannelSelect = (e) => {
-  e.preventDefault();
-  const array = e.target.value.split("%-%");
-  
+  const handleCategorySelect = (e) => {
+    e.preventDefault();
+    setInfoSubmit({ ...infoSubmit, categoryId: e.target.value });
+    setDisabled(false);
+  };
 
-  axios.get(`${URL_BASE}/category/bychannel`, {params: {schemaName: schemaName, channelId: array[1]}})
-  .then(r => {
-    console.log(r.data)
-    let array1:any[] = []
-    r.data.map(el => {
-      let obj = {
-        name: el.catName,
-        id: el.catId,
-      }
-      array1.push(obj)
-    })
-    setCategoryState(array1)
-    
-  })
-};
+  const handleDeleting = (e) => {
+    e.preventDefault();
+    let btn = document.getElementById("last-remove-btn");
+    if (e.target.value === "delete") {
+      setOpenRemove({ ...openRemove, buttonDisabled: false });
+    } else {
+      setOpenRemove({ ...openRemove, buttonDisabled: true });
+    }
+  };
 
-const handleCategorySelect = (e) => {
-  e.preventDefault();
-  setInfoSubmit({ ...infoSubmit, categoryId:e.target.value });
-  setDisabled(false)
-}
+  const [disabled, setDisabled] = useState(true);
 
-const handleDeleting = (e) => {
-  e.preventDefault();
-  let btn = document.getElementById("last-remove-btn");
-  if (e.target.value === "delete") {
-    setOpenRemove({...openRemove, buttonDisabled: false})
-  } else {
-    setOpenRemove({...openRemove, buttonDisabled: true})
-  }
-};
-
-const [disabled, setDisabled] = useState(true)
-
-return(
-    <div>     {/*====================================================================================================  */}
-      <form onSubmit={handleSubmit} className="Settings__form Select__100w" >
-      <div className="Settings__selects ">
-          <select onChange={handleWorkspaceSelect} name="schemaName" id="" className="SelectComponent Select__100w">
-            <option value="all" className="SelectComponent_option">Workspaces</option>
+  return (
+    <div>
+      {" "}
+      {/*====================================================================================================  */}
+      <form onSubmit={handleSubmit} className="Settings__form Select__100w">
+        <div className="Settings__selects ">
+          <select
+            onChange={handleWorkspaceSelect}
+            name="schemaName"
+            id=""
+            className="SelectComponent Select__100w"
+          >
+            <option value="all" className="SelectComponent_option">
+              Workspaces
+            </option>
             {auth?.user?.myWorkspaces?.map((w, i) => (
               <option
                 key={i}
                 value={
-                  auth?.user?.myWorkspaces?.length ? auth.user.myWorkspaces[i].name : ""
+                  auth?.user?.myWorkspaces?.length
+                    ? auth.user.myWorkspaces[i].name
+                    : ""
                 }
                 className="SelectComponent_option"
               >
@@ -153,21 +162,30 @@ return(
               </option>
             ))}
           </select>
-          </div>
-          <div className="Settings__selects">
+        </div>
+        <div className="Settings__selects">
           {channelsState?.length ? (
-            <select onChange={handleChannelSelect} className="SelectComponent Select__50w" name="oldName" id="">
-              <option value="all" className="SelectComponent_option">Channels</option>
+            <select
+              onChange={handleChannelSelect}
+              className="SelectComponent Select__50w"
+              name="oldName"
+              id=""
+            >
+              <option value="all" className="SelectComponent_option">
+                Channels
+              </option>
               {channelsState?.map((ch) => (
-                <option key={ch.id} value={ch.name + "%-%" + ch.id} className="SelectComponent_option">
+                <option
+                  key={ch.id}
+                  value={ch.name + "%-%" + ch.id}
+                  className="SelectComponent_option"
+                >
                   {ch.name}
                 </option>
               ))}
             </select>
           ) : null}
-          </div>
-          <div>
-          {categoryState?.length ? (
+         {categoryState?.length ? (
             <select
               onChange={handleCategorySelect}
               className="SelectComponent Select__50w"
@@ -187,30 +205,38 @@ return(
               ))}
             </select>
           ) : null}
-
-          </div>
-          <div className="Settings__inputs_container">
-          <button type="button" onClick={(e) => handleShow(e)} className='Settings__button' disabled={disabled}>
+        </div>
+        <div className="Settings__inputs_container">
+          <button
+            type="button"
+            onClick={(e) => handleShow(e)}
+            className="Settings__button"
+            disabled={disabled}
+          >
             Remove Category
           </button>
           <div className={`${openRemove.divClass}`}>
             <label className="Settings__label_delete">
               If you are sure about deleting this category type 'delete'
             </label>
-            <input 
+            <input
               type="text"
               onChange={handleDeleting}
               placeholder="deleted"
-              autoComplete='off'
+              autoComplete="off"
               className="Settings__input"
             ></input>
-            <button type="submit" id="last-remove-btn" disabled={openRemove.buttonDisabled} className='Settings__button'>
+            <button
+              type="submit"
+              id="last-remove-btn"
+              disabled={openRemove.buttonDisabled}
+              className="Settings__button"
+            >
               Confirm
             </button>
           </div>
         </div>
       </form>
-    
     </div>
-  )
-}
+  );
+};
